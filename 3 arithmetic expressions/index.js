f6cd46eb1ab2bf12ff31cd940661f6b6eecf4399
Siavash Ahmadi <sahmadi4@student.gsu.edu>
2023-02-16 17:56:45 -0500

    first 2 complete, 3rd WIP

---
+++ 3 arithmetic expressions/index.js
@@ -0,0 +1,8 @@
+/* 
+    arithmetic expression is a combination of...
+    operands (values, variables, etc.)
+    operators ( +  -  *  /  %)
+    that can be evaluated to a value
+    ex: y = x + 5;
+*/
+console.log('eat my ass');
