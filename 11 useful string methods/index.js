f7ce5a95cbea3bcc5a98c4d413b12aefe402bb8b
Siavash Ahmadi <sia2156@gmail.com>
2023-02-17 00:59:11 -0500

    finished mods 3-10, 11 WiP

---
+++ 11 useful string methods/index.js
@@ -0,0 +1,2 @@
+// useful string properties & methods
+
--- 3 arithmetic expressions/index.js
+++ 3 arithmetic expressions/index.js
@@ -5,4 +5,30 @@
     that can be evaluated to a value
     ex: y = x + 5;
 */
-console.log('eat my ass');
+
+let students = 21;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// let extraStudents = students % 2; // checking for even or odd doi!
+
+// AUGMENTED ASSIGNMENT OPERATORS
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+
+
+/*
+    operator precedence
+    1. parenthesis ()
+    2. exponents
+    3. multiplication & division
+    4. addition & subtraction
+*/
+
+let result = (1 + 2) * (3 + 4);
+
+console.log(result);
